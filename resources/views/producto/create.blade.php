8bbf89f3c56a0a6976be624351ea00d348f55214
Padawan2612 <mjoeln2612@hotmail.com>
2023-03-05 18:56:49 -0500

    panelAdministracion

--- resources/views/producto/create.blade.php
+++ resources/views/producto/create.blade.php
@@ -17,9 +17,9 @@
                     </ul>
                 </div>
             @endif
-        </div> 
+        </div>
         {!! Form::open(['route' => 'producto.store','files'=>'true']) !!}
-        {{Form::token()}} 
+        {{Form::token()}}
         <div class="form-group">
             <label  class="control-label" for="categoria_id">CATEGORÍAS</label>
             {!! Form::select('categoria_id', $categorias,null,['class' => 'form-control','placeholder' => 'Elija una categoria de producto'])!!}
@@ -41,7 +41,7 @@
         <div class="row">
             <div class="form-group col-md-6">
                 <label for="price">Precio</label>
-                <input type="number" step="any" name="price" class="form-control" required placeholder="Ingrese su Nombre">
+                <input type="number" step="any" name="price" class="form-control" required placeholder="Ingrese el precio">
             </div>
             <div class="form-group col-md-6">
                 <label for="image">Imagen:</label>
@@ -57,8 +57,8 @@
           <div class="form-group">
             {!! Form::submit('Guardar', array('class' =>'btn btn-outline-primary')) !!}
             <a href="{{ route('producto.index')}}" class="btn btn-outline-primary">ATRAS</a>
-            
-        {!! Form::close()!!}   
+
+        {!! Form::close()!!}
         </div>
     </div>
 </div>
