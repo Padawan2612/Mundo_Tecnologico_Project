dd9a0dc4ab5b7daed79928d96da1e3c276d89ad8
Alex-Xavier-Vilana-Caiza <axc.vilana@yavirac.edu.ec>
2023-03-05 14:03:27 -0500

    Cambios agregados en Panel administrativo menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,87 +121,7 @@
                         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                             data-accordion="false">
 
-                            <li class="nav-item">
-                                <a href="{{url('home')}}" class="{{ Request::path() === '/home' ? 'nav-link active' : 'nav-link' }}">
-                                    <i class="text-info  fas fa-university"></i>
-                                    <p>Inicio</p>
-                                </a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a href="{{url('usuarios')}}"
-                                    class="{{ Request::path() === 'usuarios' ? 'nav-link active ' : 'nav-link' }}">
-                                    <i class="text-info  fas fa-user-tie"></i>
-                                    <p>
-                                        Usuarios
-                                        <?php $users_count = DB::table('users')->count(); ?>
-                                        <span class="right badge badge-danger">{{ $users_count ?? '0' }}</span>
-                                    </p>
-                                </a>
-                            </li>
-
-                            <li class="nav-item admin">
-                                <a href="{{url('roles')}}"
-                                    class="{{ Request::path() === 'roles' ? 'nav-link active' : 'nav-link' }}">
-                                    <i class="text-info fas fa-user-tag"></i>
-                                    <p>
-                                        Roles
-                                    </p>
-                                </a>
-                            </li>
-
-                            <li class="nav-item admin">
-                                <a href="{{url('clientes/todas')}}"
-                                    class="{{ Request::path() === 'clientes' ? 'nav-link active' : 'nav-link' }}">
-                                    <i class="text-info fas fa-user-friends"></i>
-                                    <p>
-                                        Clientes
-                                    </p>
-                                </a>
-                            </li>
-
-                            <li class="nav-item admin">
-                                <a href="{{url('proveedores')}}"
-                                    class="{{ Request::path() === 'proveedores' ? 'nav-link active' : 'nav-link' }}">
-                                    <i class="text-info fas fa-users-cog"></i>
-                                    <p>
-                                        Proveedores
-                                    </p>
-                                </a>
-                            </li>
-
-                            <li class="nav-item admin">
-                                <a href="{{url('ofertas/todas')}}"
-                                    class="{{ Request::path() === 'ofertas/todas' ? 'nav-link active' : 'nav-link' }}">
-                                    <i class="text-info fas fa-star"></i>
-                                    <p>
-                                        Oferta del dia
-                                    </p>
-                                </a>
-                            </li>
-
-                            <li class="nav-item admin">
-                                <a href="{{url('Categorias')}}"
-                                    class="{{ Request::path() === 'Categorias' ? 'nav-link active' : 'nav-link' }}">
-                                    <i class="text-info fas fa-shopping-bag"></i>
-                                    <p>
-                                    Categorias de Producto
-                                    </p>
-                                </a>
-                            </li>
-
-                            <li class="nav-item admin">
-                                <a href="{{url('producto')}}"
-                                    class="{{ Request::path() === 'producto' ? 'nav-link active' : 'nav-link' }}">
-                                    <i class="text-info fas fa-shopping-basket"></i>
-                                    <p>
-                                    Productos
-                                    <?php $product_count = DB::table('productos')->count(); ?>
-                                        <span class="right badge badge-danger">{{ $product_count ?? '0' }}</span>
-                                    </p>
-                                </a>
-                            </li> <br> <br> <br> <br> <br> <br>
-
+                            
 
 
 
@@ -282,26 +202,7 @@
                                 </a>
                             </li>
 
-                            <li class="nav-item admin">
-                                <a href="{{url('ofertas/todas')}}"
-                                    class="{{ Request::path() === 'ofertas/todas' ? 'nav-link active' : 'nav-link' }}">
-                                    <i class="fas fa-user-friends"></i>
-                                    <p>
-                                        Ofertas del dia
-                                    </p>
-                                </a>
-                            </li>
-
-                            <li class="nav-item admin">
-                                <a href="{{url('clientes/todas')}}"
-                                    class="{{ Request::path() === 'clientes/todas' ? 'nav-link active' : 'nav-link' }}">
-                                    <i class="fas fa-user-friends"></i>
-                                    <p>
-                                        clientes
-                                    </p>
-                                </a>
-                            </li>
-
+                            
 
                             @endcan
                             @can('personal')
